1434b9e636b47200025525c4e9e8672fae1bbf96
Philip Belmonte <bastigbelmonte@gmail.com>
2022-02-13 21:33:16 -0500

    Initial Commit

---
+++ Bip The Guy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bip The Guy
+//
+//  Created by Basti Belmonte on 2/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
